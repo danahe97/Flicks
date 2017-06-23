d9a1f1d31a3bcd2e00650aa598fcc794ac706815
Dana He <danahe97@fb.com>
2017-06-23 13:48:55 -0700

    initial commit

---
+++ app/src/main/java/com/codepath/flicks/models/Movie.java
@@ -0,0 +1,41 @@
+package com.codepath.flicks.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by danahe97 on 6/22/17.
+ */
+
+public class Movie {
+
+    // values from API
+    private  String title;
+    private String overview;
+    private String posterPath; // only the path
+    private  String backdropPath;
+
+    // initialize from JSON data
+    public Movie(JSONObject object) throws JSONException {
+        title = object.getString("title");
+        overview = object.getString("overview");
+        posterPath = object.getString("poster_path");
+        backdropPath = object.getString("backdrop_path");
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+}
