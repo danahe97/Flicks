b0db56e6aca23e1a1c9209475492b1d7eedb9b4b
Dana He <danahe97@fb.com>
2017-06-30 15:32:16 -0700

    started Details activity

--- app/src/main/java/com/codepath/flicks/MovieAdapter.java
+++ app/src/main/java/com/codepath/flicks/MovieAdapter.java
@@ -113,12 +113,13 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         public ViewHolder(View itemView) {
             super(itemView);
             // lookup view object by id
-            ivPosterImage = (ImageView) itemView.findViewById(R.id.ivPosterImage);
-            ivBackdropImage = (ImageView) itemView.findViewById(R.id.ivBackdropImage);
-            tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
-            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+            ivPosterImage = itemView.findViewById(R.id.ivPosterImage);
+            ivBackdropImage = itemView.findViewById(R.id.ivBackdropImage);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
             // add this as the itemView's OnClickListener
             itemView.setOnClickListener(this);
+
         }
 
         @Override
